e88e973e1da3705c6389765901183189f8bd6b0d
dong-woogie <com6511@gmail.com>
2021-06-24 23:56:59 +0900

    ConfigModule 설정 및 Typeorm설정과 DB연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItorokModule } from './itorok/itorok.module';
+import { OauthsModule } from './oauths/oauths.module';
+import { SocialModule } from './social/social.module';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+import * as Joi from 'joi';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    SocialModule,
+    OauthsModule,
+    ItorokModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test'),
+        DB_NAME: Joi.string(),
+        DB_HOST: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_PORT: Joi.string(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: +process.env.DB_PORT,
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
