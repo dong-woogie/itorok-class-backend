f052645e20d02d3d462f0391d44a714c2a5aff5e
dong-woogie <com6511@gmail.com>
2021-06-29 20:46:21 +0900

    add user-entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,29 @@
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserProfile } from './user-profile.entity';
+
+export enum UserRole {
+  client = 'client',
+  owner = 'owner',
+}
+
+registerEnumType(UserRole, {
+  name: 'UserRole',
+});
+
+@ObjectType()
+@Entity()
+export class User extends CoreEntity {
+  @Field((type) => UserRole)
+  @Column({ type: 'enum', enum: UserRole })
+  role: UserRole;
+
+  @Field((type) => String)
+  @Column({ unique: true })
+  username: string;
+
+  @Field((type) => UserProfile)
+  @OneToOne((type) => UserProfile, (profile) => profile.user)
+  profile: UserProfile;
+}
