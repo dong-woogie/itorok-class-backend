beeda7042bd71aa033ef7150a81ba75039be0bcd
dong-woogie <com6511@gmail.com>
2021-07-13 17:02:30 +0900

    소셜로그인과 소셜회원가입 동작을 위한 social.module 생성

--- src/social/social.controller.ts
+++ src/social/social.controller.ts
@@ -1,13 +1,42 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Next,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
-
+import { SocialProvider } from 'src/users/entities/social-account.entity';
+import { SocialRegisterInputType } from './dto/register.dto';
+import { SocialService } from './social.service';
 @Controller('social')
 export class SocialController {
+  constructor(private readonly socialService: SocialService) {}
+
   @Get('kakao')
-  kakaoCallback(@Req() req: Request, @Res() res: Response) {
-    const code = req.query.code as string;
-    if (!code) return res.redirect(`http://localhost:3000/login`);
-    res.redirect(`http://localhost:3000/social?code=${code}&provider=kakao`);
+  kakaoCallback(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Query() { code, state }: { code: string; state: SocialProvider },
+  ): void {
+    this.socialService.kakaoLogin(res, { code, state });
+  }
+
+  @Post('register')
+  socialRegister(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() body: SocialRegisterInputType,
+  ) {
+    this.socialService.socialRegister(req, res, body);
+  }
+
+  @Get('profile')
+  getSocialProfile(@Req() req: Request, @Res() res: Response) {
+    this.socialService.getSocialProfileWhenRegister(req, res);
   }
 
   // @Get()
--- src/social/social.module.ts
+++ src/social/social.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { SocialAccount } from 'src/users/entities/social-account.entity';
+import { UserProfile } from 'src/users/entities/user-profile.entity';
+import { User } from 'src/users/entities/user.entity';
 import { SocialController } from './social.controller';
+import { SocialService } from './social.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, UserProfile, SocialAccount]),
+    AuthModule,
+  ],
   controllers: [SocialController],
+  providers: [SocialService],
 })
 export class SocialModule {}
