2feca19845a5d134e9bb720ca076625aed6bd29e
dong-woogie <com6511@gmail.com>
2021-07-13 16:59:00 +0900

    remove unused files

--- src/itorok/itorok.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ItorokResolver } from './itorok.resolver';
-
-@Module({
-  providers: [ItorokResolver],
-})
-export class ItorokModule {}
--- src/oauths/oauths.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OauthsController } from './oauths.controller';
-
-@Module({
-  controllers: [OauthsController],
-})
-export class OauthsModule {}
