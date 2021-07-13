e25cdff80a3c4b75eeea915eae76a97367967a11
dong-woogie <com6511@gmail.com>
2021-07-13 17:05:08 +0900

    하드코딩 하지않도록 변수에 입력

---
+++ src/common/constants.ts
@@ -0,0 +1,5 @@
+export const SECRET_KEY = 'SECRET_KEY';
+export const REDIRECT_URI = 'REDIRECT_URI';
+export const KAKAO_API_URI = 'KAKAO_API_URI';
+export const REGISTER_TOKEN = 'register_token';
+export const ACCESS_TOKEN = 'access_token';
