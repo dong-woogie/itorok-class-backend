5d72923234ee592703d2fcc9fc0afe5ed9ef2b84
dong-woogie <com6511@gmail.com>
2021-07-18 23:59:20 +0900

    getSocialProfile resolver와 service 구현

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,5 +1,10 @@
+import { Response } from 'express';
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GetCookies } from 'src/auth/get-cookies.decorator';
+import { GqlResponse } from 'src/auth/gql-response.decorator';
 import { CoreOutput } from 'src/common/dtos/output.dto';
+import { GetSocialProfileOutput } from './dtos/get-social-profile.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -7,8 +12,10 @@ import { UsersService } from './users.service';
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Query((returns) => CoreOutput)
-  findUser() {
-    return this.usersService.findAllUser();
+  @Query((returns) => GetSocialProfileOutput)
+  getSocialProfile(@GetCookies() cookies) {
+    return this.usersService.getSocialProfile(cookies);
+  }
+
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Response } from 'express';
+import { AuthService } from 'src/auth/auth.service';
+import { REGISTER_TOKEN } from 'src/common/constants';
+import { ErrorMessage } from 'src/common/dtos/output.dto';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import {
+  GetSocialProfileOutput,
+  SocialProfile,
+} from './dtos/get-social-profile.dto';
+import { LoginWithSocialInput } from './dtos/login-with-social.dto';
+import { RegisterWithSocialInput } from './dtos/register-with-social.dto';
+import { SocialAccount } from './entities/social-account.entity';
+import { UserProfile } from './entities/user-profile.entity';
+import { User, UserRole } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(SocialAccount)
+    private readonly socialAccounts: Repository<SocialAccount>,
+    @InjectRepository(UserProfile)
+    private readonly profiles: Repository<UserProfile>,
+    private readonly authService: AuthService,
   ) {}
 
-  async findAllUser() {
-    const users = await this.users.find({
-      relations: ['profile'],
-    });
 
     console.log(users);
     return { ok: true };
+  }
+  async getSocialProfile(cookies): Promise<GetSocialProfileOutput> {
+    try {
+      const token = cookies[REGISTER_TOKEN];
+      if (!token) throw new Error(ErrorMessage.NOT_FOUND_REGISTER_TOKEN);
+
+      const profile = (await this.authService.decodedToken<SocialProfile>(
+        token,
+      )) as SocialProfile;
+      return {
+        ok: true,
+        profile,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: e?.message,
+      };
+    }
   }
 }
