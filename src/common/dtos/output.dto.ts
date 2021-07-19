2f1f9fa1173b013d49a1ebde7d967b16318c3efd
dong-woogie <com6511@gmail.com>
2021-07-19 23:33:24 +0900

    custom error message

--- src/common/dtos/output.dto.ts
+++ src/common/dtos/output.dto.ts
@@ -1,10 +1,23 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
+
+export enum ErrorMessage {
+  NOT_REGISTER = 'NOT_REGISTER',
+  NOT_FOUND_REGISTER_TOKEN = 'NOT_FOUND_REGISTER_TOKEN',
+  EXPIRE_ACCESS_TOKEN = 'EXPIRE_ACCESS_TOKEN',
+  NOT_FOUND_ACCESS_TOKEN = 'NOT_FOUND_ACCESS_TOKEN',
+  DO_LOGOUT = 'DO_LOGOUT',
+  FORBIDDEN_RESOURCE = 'Forbidden resource',
+}
+
+registerEnumType(ErrorMessage, {
+  name: 'ErrorMessage',
+});
 
 @ObjectType()
 export class CoreOutput {
   @Field((type) => Boolean)
   ok: boolean;
 
-  @Field((type) => String, { nullable: true })
-  error?: string;
+  @Field((type) => ErrorMessage, { nullable: true })
+  error?: ErrorMessage;
 }
