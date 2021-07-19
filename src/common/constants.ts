a03058215ecbe25f835cbdb4aabf7e088438c7a0
dong-woogie <com6511@gmail.com>
2021-07-19 23:33:06 +0900

    변수 추가

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,6 @@ export const REDIRECT_URI = 'REDIRECT_URI';
 export const KAKAO_API_URI = 'KAKAO_API_URI';
 export const REGISTER_TOKEN = 'register_token';
 export const ACCESS_TOKEN = 'access_token';
+export const REFRESH_TOKEN = 'refresh_token';
+export const ONE_HOUR = 1000 * 60 * 60;
+export const ONE_DAY = ONE_HOUR * 24;
