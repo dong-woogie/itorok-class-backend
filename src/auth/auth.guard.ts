2f91c564217102a0811b2575f2254f018de95724
dong-woogie <com6511@gmail.com>
2021-07-22 23:13:46 +0900

    refactoring - 동작이 맞지않아서 수정 및 알맞는 에러로 변경

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -3,19 +3,15 @@ import {
   ExecutionContext,
   ForbiddenException,
   Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { InjectRepository } from '@nestjs/typeorm';
-import {
-  ACCESS_TOKEN,
-  ONE_DAY,
-  ONE_HOUR,
-  REFRESH_TOKEN,
-} from 'src/common/constants';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from 'src/common/constants';
 import { ErrorMessage } from 'src/common/dtos/output.dto';
 import { User } from 'src/users/entities/user.entity';
-import { Repository, UsingJoinColumnIsNotAllowedError } from 'typeorm';
+import { Repository } from 'typeorm';
 import { AuthService } from './auth.service';
 import { RoleType } from './role.decorator';
 
@@ -34,14 +30,16 @@ export class AuthGuard implements CanActivate {
     const roles = this.reflector.get<RoleType>('roles', context.getHandler());
     if (!roles) return true;
 
-    const accessToken = gqlContext.cookies[ACCESS_TOKEN];
-    const refreshToken = gqlContext.cookies[REFRESH_TOKEN];
+    const accessToken = gqlContext?.accessToken || '';
+    const refreshToken = gqlContext.cookies?.refresh_token || '';
 
     if (!accessToken) {
       // access_token, refresh_token 둘 다 없을 때 로그아웃 요청하기
-      if (!refreshToken) throw new ForbiddenException(ErrorMessage.DO_LOGOUT);
+      if (!refreshToken) {
+        throw new UnauthorizedException(ErrorMessage.DO_LOGOUT);
+      }
       // refresh_token은 있을 떄 refresh_token을 통해서 access_token 요청하기
-      throw new ForbiddenException(ErrorMessage.NOT_FOUND_ACCESS_TOKEN);
+      throw new UnauthorizedException(ErrorMessage.NOT_FOUND_ACCESS_TOKEN);
     }
 
     const decodedToken = await this.authService.decodedToken(accessToken);
@@ -49,20 +47,21 @@ export class AuthGuard implements CanActivate {
     const diff = decodedToken.exp * 1000 - now;
 
     // 만료기간 지남
-    if (diff > ONE_HOUR) {
+    if (diff < 0) {
       // refresh 토큰이 없거나 refresh 토큰도 만료기간이 지났으면 로그아웃
-      if (!refreshToken) throw new ForbiddenException(ErrorMessage.DO_LOGOUT);
+      if (!refreshToken)
+        throw new UnauthorizedException(ErrorMessage.DO_LOGOUT);
 
       const refreshTokenDecoded = await this.authService.decodedToken(
         refreshToken,
       );
-      const diff = refreshTokenDecoded.exp - now;
-      if (diff > ONE_DAY * 30) {
-        throw new ForbiddenException(ErrorMessage.DO_LOGOUT);
+      const diff = refreshTokenDecoded.exp * 1000 - now;
+      if (diff < 0) {
+        throw new UnauthorizedException(ErrorMessage.DO_LOGOUT);
       }
 
       // access_token만 만료되서 client에서 access_token 재요청
-      throw new ForbiddenException(ErrorMessage.EXPIRE_ACCESS_TOKEN);
+      throw new UnauthorizedException(ErrorMessage.EXPIRED_ACCESS_TOKEN);
     }
 
     // correct action
