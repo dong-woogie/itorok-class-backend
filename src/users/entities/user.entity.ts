ab10eecbec8fe05c319a057ca7ab46552fdd424a
dong-woogie <com6511@gmail.com>
2021-07-26 00:00:07 +0900

    add gender column in user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,27 @@
 import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, OneToOne } from 'typeorm';
 import { AuthToken } from './auth-token.entity';
 import { UserProfile } from './user-profile.entity';
 
 export enum UserRole {
   client = 'client',
-  owner = 'owner',
+  author = 'author',
+  manager = 'manager',
+  admin = 'admin',
+}
+
+export enum Gender {
+  male = 'male',
+  female = 'female',
 }
 
 registerEnumType(UserRole, {
   name: 'UserRole',
+});
+
+registerEnumType(Gender, {
+  name: 'Gender',
 });
 
 @ObjectType()
@@ -23,6 +34,10 @@ export class User extends CoreEntity {
   @Field((type) => String)
   @Column({ unique: true })
   username: string;
+
+  @Field((type) => Gender, { nullable: true })
+  @Column({ type: 'enum', enum: Gender, nullable: true })
+  gender?: Gender;
 
   @Field((type) => UserProfile)
   @OneToOne((type) => UserProfile, (profile) => profile.user)
